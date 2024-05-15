964e7953555886dd9f4a638139bc96e19a7b093b
Arthur <aldsio196@gmail.com>
2024-05-15 20:00:58 +0300

    verse-finn-vrode:

--- src/App.js
+++ src/App.js
@@ -1,73 +1,72 @@
-import { useState, useEffect, useMemo, useCallback, memo } from "react";
-import "../src/App.css";
-import Memory from "./Games/Memory/Memory";
-import { StartPage } from "./Pages/StartPage/StartPage";
-import { Header } from "./Components/Header/Header";
-import { SettingMemory } from "./Pages/Settings/SettingsMemory";
-import { Routes, Route } from "react-router-dom";
-import { NumbersGame } from "./Games/NumbersGame/NumbersGame";
-import { NumbersOrdersGame } from "./Games/NumbersOrdersGame/NumbersOrdersGame";
-import { Home } from "./Pages/Home/Home";
-import { Statics } from "./Pages/Statics/Statics";
-import { Cards } from "./Cards";
-import { atom } from "jotai";
-import { DIV_ } from "./Components/DIV_";
-export const atomStatickMassHistory = atom([]);
+import { atom } from 'jotai'
+import { memo, useEffect, useMemo, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import '../src/App.css'
+import { Cards } from './Cards'
+import { Header } from './Components/Header/Header'
+import Memory from './Games/Memory/Memory'
+import { NumbersGame } from './Games/NumbersGame/NumbersGame'
+import { NumbersOrdersGame } from './Games/NumbersOrdersGame/NumbersOrdersGame'
+import { Home } from './Pages/Home/Home'
+import { SettingMemory } from './Pages/Settings/SettingsMemory'
+import { StartPage } from './Pages/StartPage/StartPage'
+import { Statics } from './Pages/Statics/Statics'
+export const atomStatickMassHistory = atom([])
 export const App = memo(() => {
-  const [quanity, setQuanity] = useState(null);
-  const [gameindex, setGameindex] = useState(
-    JSON.parse(localStorage.getItem("gameindex")) || 0
-  );
-  const [timeOnSite, setTimeOnSite] = useState(
-    JSON.parse(localStorage.getItem("timeOnSite"))
-  );
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeOnSite((prev) => prev + 1000);
-      localStorage.setItem("timeOnSite", JSON.stringify(timeOnSite));
-    }, 1000);
+	const [quanity, setQuanity] = useState(null)
+	const [gameindex, setGameindex] = useState(
+		JSON.parse(localStorage.getItem('gameindex')) || 0
+	)
+	const [timeOnSite, setTimeOnSite] = useState(
+		JSON.parse(localStorage.getItem('timeOnSite'))
+	)
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setTimeOnSite(prev => prev + 1000)
+			localStorage.setItem('timeOnSite', JSON.stringify(timeOnSite))
+		}, 1000)
 
-    return () => clearInterval(timer);
-  }, [timeOnSite]);
-  const DIFF_NAMES = useMemo(() => Cards[gameindex].dificcult, [gameindex]);
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route
-          path="/SettingMemory"
-          element={
-            <SettingMemory
-              setQuanity={setQuanity}
-              gameindex={gameindex}
-              quanity={quanity}
-              DIFF_NAMES={DIFF_NAMES}
-              nameDiffCard={DIFF_NAMES}
-            />
-          }
-        ></Route>
-        <Route
-          path="/Games"
-          element={<Home setGameindex={setGameindex} />}
-        ></Route>
-        <Route
-          path="/MemoryGame"
-          element={<Memory difficult={quanity} gameindex={gameindex} />}
-        ></Route>
-        <Route
-          path="/Numbers"
-          element={<NumbersGame difficult={quanity} gameindex={gameindex} />}
-        ></Route>
-        <Route
-          path="/NumbersOrders"
-          element={
-            <NumbersOrdersGame difficult={quanity} gameindex={gameindex} />
-          }
-        ></Route>
-        <Route path="/Statics" element={<Statics />}></Route>
-      </Routes>
-    </>
-  );
-});
-export default App;
+		return () => clearInterval(timer)
+	}, [timeOnSite])
+	const DIFF_NAMES = useMemo(() => Cards[gameindex].dificcult, [gameindex])
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path='/' element={<StartPage />} />
+				<Route
+					path='/SettingMemory'
+					element={
+						<SettingMemory
+							setQuanity={setQuanity}
+							gameindex={gameindex}
+							quanity={quanity}
+							DIFF_NAMES={DIFF_NAMES}
+							nameDiffCard={DIFF_NAMES}
+						/>
+					}
+				></Route>
+				<Route
+					path='/Games'
+					element={<Home setGameindex={setGameindex} />}
+				></Route>
+				<Route
+					path='/MemoryGame'
+					element={<Memory difficult={quanity} gameindex={gameindex} />}
+				></Route>
+				<Route
+					path='/Numbers'
+					element={<NumbersGame difficult={quanity} gameindex={gameindex} />}
+				></Route>
+				<Route
+					path='/NumbersOrders'
+					element={
+						<NumbersOrdersGame difficult={quanity} gameindex={gameindex} />
+					}
+				></Route>
+				<Route path='/Statics' element={<Statics />}></Route>
+			</Routes>
+		</>
+	)
+})
+export default App
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,34 +1,34 @@
-import style from "./header.module.css";
-import { Link } from "react-router-dom";
-import { memo } from "react";
-import { DIV_ } from "../DIV_";
+import { memo } from 'react'
+import { Link } from 'react-router-dom'
+import { DIV_ } from '../DIV_'
+import style from './header.module.css'
 const links = [
-  {
-    to: "/",
-    title: "MemoryMaster",
-  },
-  {
-    to: "/Statics",
-    title: "Личный кабинет",
-  },
-  {
-    to: "/Games",
-    title: "Игры",
-  },
-];
+	{
+		to: '/',
+		title: 'MemoryMaster',
+	},
+	{
+		to: '/Statics',
+		title: 'Личный кабинет',
+	},
+	{
+		to: '/Games',
+		title: 'Игры',
+	},
+]
 
 export const Header = memo(() => {
-  return (
-    <header className={style["Header"]}>
-      <DIV_ />
-      {links.map((item, i) => {
-        return (
-          <Link key={i} to={item.to}>
-            {/* {item.img ? <img src={item.img}></img> : ""} */}
-            <p className="logo">{item.title}</p>
-          </Link>
-        );
-      })}
-    </header>
-  );
-});
+	return (
+		<header className={style['Header']}>
+			<DIV_ />
+			{links.map((item, i) => {
+				return (
+					<Link key={i} to={item.to}>
+						{/* {item.img ? <img src={item.img}></img> : ""} */}
+						<p className='logo'>{item.title}</p>
+					</Link>
+				)
+			})}
+		</header>
+	)
+})
